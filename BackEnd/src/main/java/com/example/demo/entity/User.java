0c6bf023a6588da1679ca89660d9fe59dfb1210e
Meixhan <798128414@qq.com>
2022-11-13 22:52:10 -0500

    database env setup

---
+++ BackEnd/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userid;
+ 
+    private String userName;
+    private String password;
+    private String salt;
+    private int role;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSalt() {
+		return salt;
+	}
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ENPM613BackEnd
+## Environment
+Java 17
+springboot 2.7.5
+maven 4.0.0 (3.8.6也可以）
+
+### Database
+PostgreSQL 14
+Database name: bookaholic
+password: 123456
+Create the database before running the application
+
+mvn install
