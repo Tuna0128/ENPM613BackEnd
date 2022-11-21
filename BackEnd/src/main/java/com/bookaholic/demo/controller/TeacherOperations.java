4816ea480c2865f795d8d55475b21f71df015334
skycrane98 <89813397+skycrane98@users.noreply.github.com>
2022-11-21 02:51:43 -0500

    Teacher list book

--- BackEnd/src/main/java/com/bookaholic/demo/controller/TeacherOperations.java
+++ BackEnd/src/main/java/com/bookaholic/demo/controller/TeacherOperations.java
@@ -8,10 +8,12 @@ import com.bookaholic.demo.util.ValidateBook;
 import java.util.UUID;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -72,5 +74,18 @@ public class TeacherOperations {
         } else{
             return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
         }
+    }
+    
+    @GetMapping("/listbook")
+    public ResponseEntity<?> listBook(@RequestParam("userId") UUID uuid){
+    	if(uuid==null) {
+    		Map<String, String> body = new HashMap<String, String>();
+    		body.put("message", "no userId given");
+    		return new ResponseEntity<>(body, HttpStatus.NOT_ACCEPTABLE);
+    	}
+    	List<BookEntity> list = bookService.queryBookByTeacherId(uuid);
+    	Map<String, List<BookEntity>> body = new HashMap<String, List<BookEntity>>();
+    	body.put("booklist", list);
+    	return new ResponseEntity<>(body, HttpStatus.ACCEPTED);
     }
 }
