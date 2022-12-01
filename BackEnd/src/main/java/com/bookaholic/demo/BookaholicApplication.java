bdfccc760a91a02ce3c588178a23e00b1d187fc0
Yuan Xu <ariayxu@umd.edu>
2022-11-30 21:52:58 -0500

    disable cors

--- BackEnd/src/main/java/com/bookaholic/demo/BookaholicApplication.java
+++ BackEnd/src/main/java/com/bookaholic/demo/BookaholicApplication.java
@@ -4,6 +4,9 @@ package com.bookaholic.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -13,4 +16,13 @@ public class BookaholicApplication {
 		SpringApplication.run(BookaholicApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*").allowedOrigins("*");
+			}
+		};
+	}
 }
--- BackEnd/src/main/java/com/bookaholic/demo/controller/FileOperations.java
+++ BackEnd/src/main/java/com/bookaholic/demo/controller/FileOperations.java
@@ -82,7 +82,6 @@ public class FileOperations {
 		return new ResponseEntity<>(body, HttpStatus.ACCEPTED);
 	}
 
-	@CrossOrigin(origins = "*")
 	@GetMapping("/download/{userId}/{filename:.+}")
 	public ResponseEntity<?> download(@PathVariable String userId, @PathVariable String filename){
 		Path filePath = Paths.get("").resolve("userUpload").resolve(userId).resolve(filename).normalize();
